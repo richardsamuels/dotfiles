5c092660bce118cf8fce3978b43fc76ef99f413a
Richard Samuels <richard.l.samuels@gmail.com>
2015-02-15 18:01:46 -0500

    Changes layout for work mode

--- tmux/layouts/std-retina-work.window.sh
+++ tmux/layouts/std-retina-work.window.sh
@@ -1,16 +1,17 @@
 # Set window root path. Default is `$session_root`.
 # Must be called before `new_window`.
-#window_root "~/Projects/std-retina"
+window_root "~/school/"
 
 # Create new window. If no argument is given, window name will be based on
 # layout file name.
 new_window
 
-split_h 70
+split_h 55
 select_pane 1
-split_v 28
+split_v 23
 select_pane 0
-split_v 50
+split_v 23
 
 # Set active pane.
 select_pane 2
+run_cmd "vim"
