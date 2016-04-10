44c0ba2610b7c882206639972477f7c4ee86eb26
Richard Samuels <richard.l.samuels@gmail.com>
2016-04-10 16:50:08 -0400

    shell fixes

--- shells/env.sh
+++ shells/env.sh
@@ -5,12 +5,12 @@ source ~/dotfiles/shells/env/uenv
 if [ ! -z "$ZSH_VERSION" ]; then
     source ~/dotfiles/shells/env/zshenv
 
-    if [ -f "~/dotfiles/shells/local/zshenv" ]; then
+    if [ -f "$HOME/dotfiles/shells/local/zshenv" ]; then
         source ~/dotfiles/shells/local/zshenv
     fi
 elif [ ! -z "$BASH_VERSION" ]; then
 
-    if [ -f "~/dotfiles/shells/local/bashenv" ]; then
+    if [ -f "$HOME/dotfiles/shells/local/bashenv" ]; then
         source ~/dotfiles/shells/local/bashenv
     fi
 
