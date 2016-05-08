b9c6ecaf1465ff96bcabe6bf4e8f40ccf369f67b
Richard Samuels <richard.l.samuels@gmail.com>
2016-05-08 18:43:26 -0400

    Flatten shells folder

--- shells/rc.sh
+++ shells/rc.sh
@@ -1,21 +1,22 @@
 #!/bin/sh
 
 if [ ! -z "$ZSH_VERSION" ]; then
-    source ~/dotfiles/shells/rc/urc
-    source ~/dotfiles/shells/rc/zshrc
+    source ${HOME}/dotfiles/shells/urc
+    source ${HOME}/dotfiles/shells/zshrc
 
-    if [ -f "~/dotfiles/shells/local/zshrc" ]; then
-        source ~/dotfiles/shells/local/zshrc
+    if [ -f "${HOME}/dotfiles/local/shells/zshrc" ]; then
+        source ${HOME}/dotfiles/local/shells/zshrc
     fi
+
 elif [ ! -z "$BASH_VERSION" ]; then
     # inferior shells don't separate environment and rc
-    source ~/dotfiles/shells/env.sh
-    source ~/dotfiles/shells/rc/urc
-    source ~/dotfiles/shells/rc/bashrc
+    source ${HOME}/dotfiles/shells/urc
+    source ${HOME}/dotfiles/shells/bashrc
 
-    if [ -f "~/dotfiles/shells/local/bashrc" ]; then
-        source ~/dotfiles/shells/local/bashrc
+    if [ -f "${HOME}/dotfiles/local/shells/bashrc" ]; then
+        source ${HOME}/dotfiles/local/shells/bashrc
     fi
+
 fi
 
 # vim: set ft=sh :
