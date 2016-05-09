c05dd23dcd8c90a97c95beb68cbd1c33b47d3d8d
Richard Samuels <richard.l.samuels@gmail.com>
2016-05-09 14:19:17 -0400

    Correct paths for local directory

--- shells/env.sh
+++ shells/env.sh
@@ -3,16 +3,16 @@
 source ${HOME}/dotfiles/shells/uenv
 
 if [ ! -z "$ZSH_VERSION" ]; then
-    source ${HOME}/dotfiles/shells/zshenv
+  source ${HOME}/dotfiles/shells/zshenv
 
-    if [ -f "${HOME}/dotfiles/shells/local/zshenv" ]; then
-        source ${HOME}/dotfiles/shells/local/zshenv
-    fi
+  if [ -f "${HOME}/dotfiles/local/zsh/zshenv" ]; then
+    source ${HOME}/dotfiles/local/zsh/zshenv
+  fi
 
 elif [ ! -z "$BASH_VERSION" ]; then
-    if [ -f "${HOME}/dotfiles/shells/local/bashenv" ]; then
-        source ${HOME}/dotfiles/shells/local/bashenv
-    fi
+  if [ -f "${HOME}/dotfiles/local/bash/bashenv" ]; then
+    source ${HOME}/dotfiles/local/bash/bashenv
+  fi
 
 fi
 
--- shells/rc.sh
+++ shells/rc.sh
@@ -4,8 +4,8 @@ if [ ! -z "$ZSH_VERSION" ]; then
     source ${HOME}/dotfiles/shells/urc
     source ${HOME}/dotfiles/shells/zshrc
 
-    if [ -f "${HOME}/dotfiles/local/shells/zshrc" ]; then
-        source ${HOME}/dotfiles/local/shells/zshrc
+    if [ -f "${HOME}/dotfiles/local/zsh/zshrc" ]; then
+        source ${HOME}/dotfiles/local/zsh/zshrc
     fi
 
 elif [ ! -z "$BASH_VERSION" ]; then
@@ -13,8 +13,8 @@ elif [ ! -z "$BASH_VERSION" ]; then
     source ${HOME}/dotfiles/shells/urc
     source ${HOME}/dotfiles/shells/bashrc
 
-    if [ -f "${HOME}/dotfiles/local/shells/bashrc" ]; then
-        source ${HOME}/dotfiles/local/shells/bashrc
+    if [ -f "${HOME}/dotfiles/local/bash/bashrc" ]; then
+        source ${HOME}/dotfiles/local/bash/bashrc
     fi
 
 fi
