4e3560ffac26edf0aef99160ecfd84463f8a0ee6
Richard Samuels <richard.l.samuels@gmail.com>
2016-05-10 09:31:33 -0400

    Add support for local pre-environment and pre-rc

--- shells/env.sh
+++ shells/env.sh
@@ -1,7 +1,27 @@
 #!/bin/sh
 
+# source pre-environment if needed
+if [ -f "${HOME}/dotfiles/local/zsh/pre.uenv" ]; then
+  source ${HOME}/dotfiles/local/shells/pre.uenv
+fi
+
+if [ ! -z "$ZSH_VERSION" ]; then
+
+  if [ -f "${HOME}/dotfiles/local/zsh/pre.zshenv" ]; then
+    source ${HOME}/dotfiles/local/zsh/pre.zshenv
+  fi
+
+elif [ ! -z "$BASH_VERSION" ]; then
+
+  if [ -f "${HOME}/dotfiles/local/bash/pre.bashenv" ]; then
+    source ${HOME}/dotfiles/local/bash/pre.bashenv
+  fi
+fi
+
+# Source Universal Environment
 source ${HOME}/dotfiles/shells/uenv
 
+# Source Shell-specific environment
 if [ ! -z "$ZSH_VERSION" ]; then
   source ${HOME}/dotfiles/shells/zshenv
 
--- shells/rc.sh
+++ shells/rc.sh
@@ -1,21 +1,42 @@
 #!/bin/sh
 
-if [ ! -z "$ZSH_VERSION" ]; then
-    source ${HOME}/dotfiles/shells/urc
-    source ${HOME}/dotfiles/shells/zshrc
+if [ -f "${HOME}/dotfiles/local/zsh/pre.urc" ]; then
+  source ${HOME}/dotfiles/local/shells/pre.urc
+fi
 
-    if [ -f "${HOME}/dotfiles/local/zsh/zshrc" ]; then
-        source ${HOME}/dotfiles/local/zsh/zshrc
-    fi
+if [ ! -z "$ZSH_VERSION" ]; then
+  if [ -f "${HOME}/dotfiles/local/zsh/pre.zshrc" ]; then
+    source ${HOME}/dotfiles/local/zsh/pre.zshrc
+  fi
 
 elif [ ! -z "$BASH_VERSION" ]; then
-    # inferior shells don't separate environment and rc
-    source ${HOME}/dotfiles/shells/urc
-    source ${HOME}/dotfiles/shells/bashrc
+  if [ -f "${HOME}/dotfiles/local/bash/pre.bashrc" ]; then
+    source ${HOME}/dotfiles/local/bash/pre.bashrc
+  fi
 
-    if [ -f "${HOME}/dotfiles/local/bash/bashrc" ]; then
-        source ${HOME}/dotfiles/local/bash/bashrc
-    fi
+fi
+
+# Source universal RC
+source ${HOME}/dotfiles/shells/urc
+
+if [ ! -z "$ZSH_VERSION" ]; then
+  source ${HOME}/dotfiles/shells/zshrc
+
+  if [ -f "${HOME}/dotfiles/local/zsh/zshrc" ]; then
+    source ${HOME}/dotfiles/local/zsh/zshrc
+  fi
+
+elif [ ! -z "$BASH_VERSION" ]; then
+  # inferior shells don't separate environment and rc
+  # LANG is set in uenv, so source that if it hasn't been set
+  if [ -z "${LANG}" ]; then
+    source ${HOME}/dotfiles/shells/env.sh 
+  fi
+  source ${HOME}/dotfiles/shells/bashrc
+
+  if [ -f "${HOME}/dotfiles/local/bash/bashrc" ]; then
+    source ${HOME}/dotfiles/local/bash/bashrc
+  fi
 
 fi
 
